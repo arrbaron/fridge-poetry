982bb5601b2d7b480d78a4d3a1ace3852289475b
Roy Baron <baron.roy@gmail.com>
2017-11-28 09:33:42 -0500

    access mongodb from get routes

--- public/js/html-renderer.js
+++ public/js/html-renderer.js
@@ -13,7 +13,7 @@ const HTMLRenderer = {
   },
 
   displayFridge: function(fridge) {
-    this.displayWordBank(App.getRandomWords(MAGNETS, 3));
+    this.displayWordBank(App.getRandomWords(WORD_BANK, 3));
     this.displayPoem(fridge);
   },
 
