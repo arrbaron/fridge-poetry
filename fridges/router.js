15064bb26f8c9607637047fe5e3f20af0e936c14
Roy Baron <baron.roy@gmail.com>
2017-12-04 15:28:25 -0500

    add save fridge auth and CORS handling

--- fridges/router.js
+++ fridges/router.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const {Fridge} = require("./models");
 
 const router = express.Router();
 
 router.use(bodyParser.json());
 
-// get all words in the word bank
-router.get("/word-bank", (req, res) => {
-  
-});
-
 // get all fridges - no auth
 router.get("/", (req, res) => {
-  Fridge.find(function (err, fridges) {
-    if (err) return console.error(err);
-    console.log(fridges);
-  });
+  // Fridge.find(function (err, fridges) {
+  //   if (err) return console.error(err);
+  //   console.log(fridges);
+  // });
+  console.log("get all fridges");
+  res.send("get all fridges");
 });
 
+const jwtAuth = passport.authenticate("jwt", {session: false});
+
 // save a fridge - auth
+router.post("/", jwtAuth, (req, res) => {
+  console.log("saving fridge");
+  console.log(req.body.poem);
+  console.log(req.body.wordBank)
+});
 
 module.exports = { router };
--- server.js
+++ server.js
@@ -10,8 +10,20 @@ const {PORT, DATABASE_URL} = require("./config");
 
 const app = express();
 
+console.log(fridgeRouter);
+
 app.use(express.static("public"));
 app.use(morgan('common'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
